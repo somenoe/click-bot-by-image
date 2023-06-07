0af4b7889c2e80a1d232d63195d6a1fb91e54466
somenoe <otashi24@gmail.com>
2023-06-07 21:35:21 +0700

    ✨ loop check & bot type

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import pyautogui
-import cv2
-# import time
-import numpy as np
 # import sys
+import time
+import string
+import cv2
+import numpy as np
 import os
+import pyautogui
 
+TARGET_FOLDER = "./targets"
 
 
 def capture_reference_image():
@@ -19,21 +21,43 @@ def capture_reference_image():
     return screenshot_array
 
 
-def capture_reference_image_and_save():
-    # Capture the screen and save it as the reference image
-    screenshot = pyautogui.screenshot()
-    screenshot.save('reference.png')
-    print("Reference image captured successfully.")
-
-def perform_image_recognition_and_click(target):
-    # Load the reference and target images
-    # reference_image = cv2.imread('reference.png')
-    reference_image = capture_reference_image()
-    target_image = cv2.imread(target)
-
+def template_matching(
+    reference_image,
+    target_image,
+):
     # Perform template matching
-    result = cv2.matchTemplate(reference_image, target_image, cv2.TM_CCOEFF_NORMED)
+    result = cv2.matchTemplate(reference_image, target_image, cv2.TM_CCORR_NORMED)
+    # Find the best match
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+    # Calculate the percent match
+    percent_match = max_val * 100
+
+    return percent_match, max_loc
+
+
+def perform_image_recognition_and_click(file_name: string):
+    # Load the reference and target images
+    target_image = cv2.imread(f"{TARGET_FOLDER}/{file_name}")
+
+    runtimes = 0
+    MAX_RUNTIMES = 15
+    MIN_MATCH_PERCENT = 90
+    percent_match = 0
+    while percent_match <= MIN_MATCH_PERCENT:
+        reference_image = capture_reference_image()
+
+        percent_match, max_loc = template_matching(reference_image, target_image)
+        print(f"@{file_name} Percent match: {percent_match:.2f}%")
+
+        if percent_match <= MIN_MATCH_PERCENT:
+            print(f"@{file_name} Not found, wait 1 sec and try again.")
+            runtimes += 1
+            if runtimes >= MAX_RUNTIMES:
+                print(f"@{file_name} Not found, 10 times, stop.")
+                return
+            # wait 1 sec
+            time.sleep(1)
 
     # Get the coordinates of the best match
     top_left = max_loc
@@ -42,62 +66,57 @@ def perform_image_recognition_and_click(target):
     click_x = top_left[0] + (target_image.shape[1] // 2)
     click_y = top_left[1] + (target_image.shape[0] // 2)
 
+    # Draw a rectangle around the target on a copy of the reference image
+    image_with_rectangle = reference_image.copy()
+    bottom_right = (
+        top_left[0] + target_image.shape[1],
+        top_left[1] + target_image.shape[0],
+    )
+    cv2.rectangle(image_with_rectangle, top_left, bottom_right, (0, 0, 255), 2)
+
+    # Save the image with the rectangle
+    cv2.imwrite(f"./result/{file_name}.png", image_with_rectangle)
+
     # Simulate a click at the best match location
     pyautogui.click(click_x, click_y)
 
-    print(f"@{target} Clicked at location: ({click_x}, {click_y})")
+    print(f"@{file_name} Clicked at location: ({click_x}, {click_y})")
 
 
-# # Capture the reference image
-# capture_reference_image()
+def bot_type(text: string):
+    pyautogui.typewrite(text)
 
-# # Wait for some time to change the screen state (e.g., switch to the target program)
-# time.sleep(5)
 
-# # Perform image recognition and click
-# perform_image_recognition_and_click('target.png')
-# perform_image_recognition_and_click('target2.png')
+def perform_image_recognition_on_folder():
+    files = os.listdir(TARGET_FOLDER)
 
-# function read file int the folder
-def perform_image_recognition_on_folder(path):
-    files = os.listdir(path)
-    
     if len(files) == 0:
         print("No file in the target folder.")
         print("Please put the target image in the target folder.")
-        
-    
+
     # sort file by name
     files.sort()
     print("Files in the target folder: ", files)
-    
+
     for file in files:
-        print(file)
-        perform_image_recognition_and_click(f'{path}/{file}')
-        
-  
-# # Perform image recognition and click
-# perform_image_recognition_on_folder('./targets')
-
-# main function with read argument
+        # perform_image_recognition_and_click(file)
+        print(f"{file>20}")
 
 
-
-        
-TARGET_FOLDER = './targets'
 def init():
     # generate target folder
     if not os.path.exists(TARGET_FOLDER):
         os.makedirs(TARGET_FOLDER)
         print("Init successfully.")
-            
-            
+
+
 def main():
     init()
-    perform_image_recognition_on_folder(TARGET_FOLDER)
-    
+    perform_image_recognition_on_folder()
+
+
 main()
-    
+
 # TODO: add function wait until the image appear, recheck every 1 sec
 # TODO: add function to handle acion in file name ([order]_[action]_[detail].png)
 # ex. 1_click_sumbit.png, 2_wait_for_upload.png, 3_click_close_page.png
